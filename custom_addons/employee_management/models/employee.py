7aec5295a5c0e6d8cf7152a4fefe833b813b7123
Lê Quỳnh Phương <147700660+lequynhphuong@users.noreply.github.com>
2026-05-20 16:43:22 +0700

    Delete custom_addons/employee_management directory

--- custom_addons/employee_management/models/employee.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-class Employee(models.Model):
-    _name = 'employee.management'
-    _description = 'Employee Management'
-
-    name = fields.Char(string='Tên', required=True)
-    age = fields.Integer(string='Tuổi')
-    department = fields.Char(string='Phòng Ban')
-    
