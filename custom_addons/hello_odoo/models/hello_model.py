1ad8d60910a5958fbfc1027f0d7bbd69dc2d58a9
Lê Quỳnh Phương <147700660+lequynhphuong@users.noreply.github.com>
2026-05-20 16:43:33 +0700

    Delete custom_addons/hello_odoo directory

--- custom_addons/hello_odoo/models/hello_model.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-class HelloOdoo (models.Model):
-    _name = 'hello.odoo'
-    _description = 'Hello Odoo Model'
-    
-    name = fields.Char(string='Ten', required=True, default='Hello Odoo')
